18ef5115fabb3c0f7528b37916eebc46eb7ed2cf
mumbler6 <47118893+mumbler6@users.noreply.github.com>
2022-05-25 22:26:45 -0400

    added player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,24 @@
 extends KinematicBody2D
 
+const speed = 250
+var velocity = Vector2()
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	elif Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	else:
+		velocity.y = 0
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	else:
+		velocity.x = 0
+	
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+	
+	velocity = move_and_slide(velocity)
+	
