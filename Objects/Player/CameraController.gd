9ec5150121ea5ca2268d8d7491232c8f72af5b45
mumbler6 <47118893+mumbler6@users.noreply.github.com>
2022-06-05 17:24:15 -0400

    added zoomable camera and basic ingredients

---
+++ Objects/Player/CameraController.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+var zoom_speed = Vector2(.2, .2)
+var zoom_max = Vector2(2, 2)
+var zoom_min = Vector2(.6, .6)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("scroll_up") and zoom > zoom_min:
+		zoom -= zoom_speed
+	if Input.is_action_just_released("scroll_down") and zoom < zoom_max:
+		zoom += zoom_speed
