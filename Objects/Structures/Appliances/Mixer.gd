406f7a9332eddb6b1a1c3702c6503b2577f05238
mumbler6 <47118893+mumbler6@users.noreply.github.com>
2022-06-08 17:20:23 -0400

    improved mixer to make cookie even if has extra ingredients

--- Objects/Structures/Appliances/Mixer.gd
+++ Objects/Structures/Appliances/Mixer.gd
@@ -8,30 +8,46 @@ var recipe_dict = {"basic": {"flour": 1, "eggs": 2, "butter": 1, "sugar": 1}}
 # should be able to change what cookie you are making
 var setting = "basic"
 
-# variable of what ingredients are currently inside the mixer
 var current_ingredients = {}
 
 func _process(delta: float) -> void:
 	var current_recipe = recipe_dict[setting]
 	
-	if current_ingredients.hash() == current_recipe.hash():
-		current_ingredients = {}
+	if has_enough(current_recipe):
+		make_cookie(current_recipe)
 		print("make cookie")
-		
-		# create a new cookie node and move it out
 
+# checks if current_ingredients has equal or more than required
+func has_enough(current_recipe) -> bool:
+	if current_ingredients.size() != current_recipe.size():
+		return false
+	if current_ingredients.hash() == current_recipe.hash():
+		return true
+		
+	for key in current_ingredients.keys():
+		if current_ingredients[key] < current_recipe[key]:
+			return false
+			
+	return true
+
+# subtract only what you need, can also spawn cookie in this function
+func make_cookie(current_recipe) -> void:
+	for key in current_ingredients.keys():
+		current_ingredients[key] -= current_recipe[key]
+
+# adds ingredient to current_ingredients
 func add_ingredient(body_class) -> void:
 	if current_ingredients.has(body_class):
 		current_ingredients[body_class] += 1
 	else:
 		current_ingredients[body_class] = 1
-	
+
 func _on_Mixer_body_entered(body: Node) -> void:
 	var body_class = body.get_class()
 	add_ingredient(body_class)
 	print(current_ingredients)
 	body.queue_free()
 
-
+# spawned cookie needs to stop after leaving
 func _on_Mixer_body_exited(body: Node) -> void:
 	body.added_velocity = 0
