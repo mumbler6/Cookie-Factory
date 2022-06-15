e55adf9902daa6844b6df401f2f106548e84fa3e
mumbler6 <47118893+mumbler6@users.noreply.github.com>
2022-06-15 01:11:38 -0400

    mixer takes time to make cookie

--- Objects/Structures/Appliances/Mixer.gd
+++ Objects/Structures/Appliances/Mixer.gd
@@ -7,17 +7,19 @@ var recipe_dict = {"basic": {"flour": 1, "eggs": 2, "butter": 1, "sugar": 1}}
 
 # should be able to change what cookie you are making
 var setting = "basic"
+var current_recipe = recipe_dict[setting]
+onready var timer = get_node("Timer")
 
+var is_idle = true
 var input = {}
 var output = {}
 
-func _process(_delta: float) -> void:
-	var current_recipe = recipe_dict[setting]
-	if has_enough(current_recipe):
-		make_product(current_recipe)
-		
-		# if there is an output conveyor belt nearby...
-		empty_output()
+# adds body to map
+func add_object(body_class, dict) -> void:
+	if dict.has(body_class):
+		dict[body_class] += 1
+	else:
+		dict[body_class] = 1
 
 # checks if input has equal or more than required
 func has_enough(current_recipe) -> bool:
@@ -48,13 +50,20 @@ func empty_output() -> void:
 	product.is_product = true
 	get_tree().get_root().add_child(product)
 	print(output)
+
+func _on_Timer_timeout() -> void:
+	print("make cookie")
+	timer.stop()
+	make_product(current_recipe)
 	
-# adds body to map
-func add_object(body_class, dict) -> void:
-	if dict.has(body_class):
-		dict[body_class] += 1
-	else:
-		dict[body_class] = 1
+	# if there is an output conveyor belt nearby...
+	empty_output()
+	is_idle = true
+
+func _process(_delta: float) -> void:
+	if has_enough(current_recipe) and is_idle:
+		timer.start()
+		is_idle = false
 
 func _on_Mixer_body_entered(body: Node) -> void:
 	# dont accidentally delete product
@@ -66,3 +75,5 @@ func _on_Mixer_body_entered(body: Node) -> void:
 
 func _on_Mixer_body_exited(body: Node) -> void:
 	body.is_product = false
+
+
---
+++ Objects/Structures/Appliances/Timer.gd
@@ -0,0 +1,4 @@
+extends Timer
+
+func _ready() -> void:
+	wait_time = 3.0
